8f86cc432400602de23793e81337f1c60721d1b1
sapnadeshmukh <sapnad18@navgurukul.org>
2021-04-27 12:26:39 +0530

    using html and js made calculator

---
+++ form.js
@@ -0,0 +1,29 @@
+function ForMultiply()
+     {
+        num1 = document.getElementById("first number").value;
+        num2 = document.getElementById("second number").value;
+        document.getElementById("result").innerHTML = num1 * num2;
+    }
+ 
+    function ForDivide() 
+    { 
+        num1 = document.getElementById("first number").value;
+        num2 = document.getElementById("second number").value;
+        document.getElementById("result").innerHTML = num1 / num2;
+    } 
+    function ForAdd() 
+    { 
+        num1 = document.getElementById("first number").value;
+        num2 = document.getElementById("second number").value;
+        document.getElementById("result").innerHTML = Number(num1) + Number(num2);
+    } 
+
+    function ForSubstract() 
+    { 
+        num1 = document.getElementById("first number").value;
+        num2 = document.getElementById("second number").value;
+        document.getElementById("result").innerHTML = num1 - num2;
+    } 
+
+
+
